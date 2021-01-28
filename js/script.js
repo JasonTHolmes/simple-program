d745bc4fbd19aa0d1b14803e76da43cadf558610
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-01-28 07:02:45 -0800

    Corrected typographical errors

--- js/script.js
+++ js/script.js
@@ -8,5 +8,5 @@ numtwo.addEventListener('input', add);
 function add() {
     var one = parseFloat(numone.value) || 0;
     var two = parseFloat(numtwo.value) || 0;
-    addsum.innerText = one*two;
+    addsum.innerText = one+two;
     }
